682778a815fc653dee0a9472d098aed42b998599
François Vanderborght <fv@MacBook-Pro-de-Francois.local>
2014-08-06 20:39:05 +0200

    refactoring + add limit + manage product and product template exception

--- tk_export_xml/tk_export_xml.py
+++ tk_export_xml/tk_export_xml.py
@@ -6,7 +6,7 @@
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
+# published by the Free Software Foundation, either version 3 of the
 #    License, or (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
@@ -37,6 +37,7 @@ logger = logging.getLogger(__name__)
 
 FORMAT_ID = '__export__%s_%s'
 
+
 class tk_export_xml(orm.Model):
     _name = 'tk.export.xml'
 
@@ -44,6 +45,7 @@ class tk_export_xml(orm.Model):
         # Objects
         field_obj = self.pool.get('ir.model.fields')
         data_obj = self.pool.get('ir.model.data')
+        model_obj = self.pool.get('ir.model')
 
         document = OrderedDict()
 
@@ -53,17 +55,22 @@ class tk_export_xml(orm.Model):
             query = '''
                 select name from ir_model_fields f, tk_field_record r
                 where f.id = r.field_id
-                and r.action in ('run_classic', 'run_recursive')
+                and r.action = 'enabled'
                 and export_id = %s
             '''
             cr.execute(query, (export.id, ))
             field_names = map(lambda x: x[0], cr.fetchall())
 
             if 'parent_id' in field_names:
-                data_ids = self.pool.get(export.model_id.model).search(cr, uid, domain, context=context, order='parent_id DESC')
+                if export.limit > 0:
+                    data_ids = self.pool.get(export.model_id.model).search(cr, uid, domain, context=context, order='parent_id DESC', limit=export.limit)
+                else:
+                    data_ids = self.pool.get(export.model_id.model).search(cr, uid, domain, context=context, order='parent_id DESC')
             else:
-                data_ids = self.pool.get(export.model_id.model).search(cr, uid, domain, context=context)
-
+                if export.limit > 0:
+                    data_ids = self.pool.get(export.model_id.model).search(cr, uid, domain, context=context, limit=export.limit)
+                else:
+                    data_ids = self.pool.get(export.model_id.model).search(cr, uid, domain, context=context)
 
             logger.debug(str(field_names))
 
@@ -73,21 +80,24 @@ class tk_export_xml(orm.Model):
                 if xml_ids:
                     if export.fetch_existant:
                         xml_data = data_obj.browse(cr, uid, xml_ids[0])
-                        export_id = '%s.%s,%s' % (xml_data.module, xml_data.name,export.model_id.model)
+                        export_id = '%s.%s,%s' % (xml_data.module, xml_data.name, export.model_id.model)
                     else:
                         continue
                 else:
-                    export_id = FORMAT_ID % (export.model_id.model.replace('.', '_'), '%s,%s' %(data_record.get('id'), export.model_id.model))
+                    export_id = FORMAT_ID % (export.model_id.model.replace('.', '_'), '%s,%s' % (data_record.get('id'), export.model_id.model))
 
                 logger.debug('Try to export %s' % export_id)
                 if export_id in document.keys():
                     continue
-                document[export_id]= {}
+                document[export_id] = {}
                 for field_name in field_names:
                     value = data_record.get(field_name)
 
                     # Get info on relation
-                    field_id = field_obj.search(cr, uid, [('name', '=', field_name), ('model_id', '=', export.model_id.id)], limit=1)[0]
+                    if export.model_id.model == 'product.product':
+                        field_id = field_obj.search(cr, uid, [('name', '=', field_name), ('model_id', 'in', [export.model_id.id, model_obj.search(cr, uid, [('model', '=', 'product.template')])[0]])], limit=1)[0]
+                    else:
+                        field_id = field_obj.search(cr, uid, [('name', '=', field_name), ('model_id', '=', export.model_id.id)], limit=1)[0]
                     field_relation = field_obj.read(cr, uid, field_id, ['relation']).get('relation')
 
                     if type(value).__name__ == 'tuple':
@@ -95,10 +105,7 @@ class tk_export_xml(orm.Model):
                         data_ids = data_obj.search(cr, uid, [('model', '=', field_relation), ('res_id', '=', res_id)], limit=1)
                         if data_ids:
                             data = data_obj.browse(cr, uid, data_ids[0])
-                            if data.module != '__export__':
-                                value = '__xml_data__%s.%s' % (data.module, data.name)
-                            else:
-                                value = FORMAT_ID % (field_relation.replace('.', '_'), value[0])
+                            value = '__xml_data__%s.%s' % (data.module, data.name)
                         else:
                             value = FORMAT_ID % (field_relation.replace('.', '_'), value[0])
                         document[export_id][field_name] = value
@@ -109,10 +116,7 @@ class tk_export_xml(orm.Model):
                             data_ids = data_obj.search(cr, uid, [('model', '=', field_relation), ('res_id', '=', res_id)], limit=1)
                             if data_ids:
                                 data = data_obj.browse(cr, uid, data_ids[0])
-                                if data.module != '__export__':
-                                    ref = 'ref(\'__xml_data__%s.%s\')' % (data.module, data.name)
-                                else:
-                                    ref = 'ref(\'__export__%s_%s\')' % (field_relation.replace('.', '_'), res_id)
+                                ref = 'ref(\'__xml_data__%s.%s\')' % (data.module, data.name)
                             else:
                                 ref = 'ref(\'__export__%s_%s\')' % (field_relation.replace('.', '_'), res_id)
 
@@ -127,13 +131,11 @@ class tk_export_xml(orm.Model):
                     else:
                         document[export_id][field_name] = value
 
-
-
         return document
 
     @staticmethod
     def getItemLevel(item):
-            return item.getElementsByTagName("parent_id")[0].data
+        return item.getElementsByTagName("parent_id")[0].data
 
     def export_xml(self, cr, uid, ids, context=None):
         dico = False
@@ -160,12 +162,12 @@ class tk_export_xml(orm.Model):
                 print '----------'
                 value = u'' + dico[key][field_key]
                 if 'ref(\'__export__' in value or 'ref(\'__xml_data__' in value:
-                    field = etree.Element('field', name=field_key, eval=value.replace('__xml_data__',''))
+                    field = etree.Element('field', name=field_key, eval=value.replace('__xml_data__', ''))
                 elif '__export__' in value:
                     field = etree.Element('field', name=field_key, ref=value)
                     data_keys.append((value, field))
                 elif '__xml_data__' in value:
-                    field = etree.Element('field', name=field_key, ref=value.replace('__xml_data__',''))
+                    field = etree.Element('field', name=field_key, ref=value.replace('__xml_data__', ''))
                 elif 'bool,' in value:
                     value = value.split(',')[1]
                     if value == 'True':
@@ -179,7 +181,6 @@ class tk_export_xml(orm.Model):
             data.append(record)
         openerp.append(data)
 
-
         xml = etree.tostring(openerp, encoding='UTF-8', xml_declaration=True, pretty_print=True)
         data = base64.encodestring(xml)
         self.write(cr, uid, ids, {
@@ -188,17 +189,18 @@ class tk_export_xml(orm.Model):
         })
 
         return True
-    def set_all_to_ignore(self, cr, uid, ids, context=None):
+
+    def set_all_to_disabled(self, cr, uid, ids, context=None):
         query = '''
-            UPDATE tk_field_record SET action = 'ignore' where export_id = any(%s)
+            UPDATE tk_field_record SET action = 'disabled' where export_id = any(%s)
         '''
         cr.execute(query, (ids, ))
         cr.commit()
         return True
 
-    def set_all_to_run(self, cr, uid, ids, context=None):
+    def set_all_to_enabled(self, cr, uid, ids, context=None):
         query = '''
-            UPDATE tk_field_record SET action = 'run_classic' where export_id = any(%s)
+            UPDATE tk_field_record SET action = 'enabled' where export_id = any(%s)
         '''
         cr.execute(query, (ids, ))
         cr.commit()
@@ -207,13 +209,13 @@ class tk_export_xml(orm.Model):
     def onchange_model_id(self, cr, uid, ids, name, model_id, context=None):
 
 
-
         if model_id:
             # Objects
             record_obj = self.pool.get('tk.field.record')
             field_obj = self.pool.get('ir.model.fields')
+            model_obj = self.pool.get('ir.model')
 
-
+            fields_to_ignore = ['id', 'create_date', 'write_date', 'create_uid', 'write_uid']
 
             # Get record associated to current export
             record_ids = record_obj.search(cr, uid, [('export_id', 'in', ids)])
@@ -227,79 +229,30 @@ class tk_export_xml(orm.Model):
             # Create new records
             field_record_ids = []
 
-            for field_id in field_ids:
-                field_record_ids.append(
-                    record_obj.create(cr, uid, {
-                        'model_id': model_id,
-                        'field_id': field_id}))
+            for field in field_obj.browse(cr, uid, field_ids):
+                if field.name not in fields_to_ignore:
+                    field_record_ids.append(
+                        record_obj.create(cr, uid, {
+                            'model_id': model_id,
+                            'field_id': field.id,
+                        }))
+
+            if model_id == model_obj.search(cr, uid, [('model', '=', 'product.product')])[0]:
+                product_template_model_id = model_obj.search(cr, uid, [('model', '=', 'product.template')])[0]
+                field_ids = field_obj.search(cr, uid, [('model_id', '=', product_template_model_id)])
+                for field in field_obj.browse(cr, uid, field_ids):
+                    if field.name not in fields_to_ignore:
+                        field_record_ids.append(
+                            record_obj.create(cr, uid, {
+                                'model_id': product_template_model_id,
+                                'field_id': field.id}))
 
             return {
                 'value': {
-                    'field_record_ids': field_record_ids
+                    'field_record_ids': field_record_ids,
+                    'model_name': model_obj.read(cr, uid, model_id, ['model'])['model']
                 }
             }
-
-    def create(self, cr, uid, values, context=None):
-        # Object
-        field_obj = self.pool.get('tk.field.record')
-        model_obj = self.pool.get('ir.model')
-
-        # Get all lines
-        all_field_ids = field_obj.search(cr, uid, [])
-        all_fields = field_obj.browse(cr, uid, all_field_ids, context=context)
-
-
-
-        fields_to_update_ids = []
-        for field in all_fields:
-            if field.field_id.ttype in ['many2one', 'one2many', 'many2many']:
-                model_rel_ids = model_obj.search(cr, uid, [('model', '=', field.field_id.relation)])
-
-                if values['model_id'] in model_rel_ids:
-                    fields_to_update_ids.append(field.id)
-        if fields_to_update_ids:
-            query = '''
-                UPDATE tk_field_record SET action = 'run_recursive' where id = any(%s)
-            '''
-            cr.execute(query, (fields_to_update_ids, ))
-            cr.commit()
-
-        return super(tk_export_xml, self).create(cr, uid, values, context=context)
-
-
-    def unlink(self, cr, uid, ids, context=None):
-        # Object
-        field_obj = self.pool.get('tk.field.record')
-        model_obj = self.pool.get('ir.model')
-
-        # Delete line attached to exports
-        line_to_delete_ids = field_obj.search(cr, uid, [('export_id', 'in', ids)])
-        field_obj.unlink(cr, uid, line_to_delete_ids)
-
-        # Get all lines
-        all_field_ids = field_obj.search(cr, uid, [])
-        all_fields = field_obj.browse(cr, uid, all_field_ids, context=context)
-
-
-
-        for export_config in self.browse(cr, uid, ids, context):
-            fields_to_update_ids = []
-            for field in all_fields:
-                if field.field_id.ttype in ['many2one', 'one2many', 'many2many']:
-                    model_rel_ids = model_obj.search(cr, uid, [('model', '=', field.field_id.relation)])
-
-                    if export_config.model_id.id in model_rel_ids:
-                        fields_to_update_ids.append(field.id)
-
-            if fields_to_update_ids:
-                query = '''
-                    UPDATE tk_field_record SET action = 'run_classic' where id = any(%s)
-                '''
-                cr.execute(query, (fields_to_update_ids, ))
-                cr.commit()
-
-        return super(tk_export_xml, self).unlink(cr, uid, ids, context=context)
-
 
     _columns = {
         'name': fields.char('Name', size=255, required=True),
@@ -307,8 +260,10 @@ class tk_export_xml(orm.Model):
         'field_record_ids': fields.one2many('tk.field.record', 'export_id', string='Records'),
         'file': fields.binary('File'),
         'filename': fields.char('Filename', size=128),
-        'domain_custom': fields.char('Domain', size=255),
-        'fetch_existant': fields.boolean('Fetch also in ir.model.data')
+        'domain_custom': fields.char('Domain', size=255, required=True),
+        'fetch_existant': fields.boolean('Get ID from database'),
+        'limit': fields.integer('Limit'),
+        'model_name': fields.char('Model Name', type='char', size=255)
     }
 
     _defaults = {
@@ -323,62 +278,24 @@ class tk_field_record(orm.Model):
     _name = 'tk.field.record'
 
     _actions = (
-        ('ignore', 'Ignore'),
-        ('run_classic', 'Run'),
-        ('run_recursive', 'Run with children')
+        ('disabled', 'Disabled'),
+        ('enabled', 'Enabled'),
     )
-
-    def _get_default_action(self, cr, uid, ids, field_name, arg, context=None):
-        res = {}
-        for record in self.browse(cr, uid, ids, context=context):
-            if not record.action:
-                # If relation type we check if there is export configuration for the model associated
-                if record.field_id.name in ['id', 'write_date', 'create_date']:
-                    res[record.id] = 'ignore'
-                elif record.field_type in ['one2many', 'many2one', 'many2many']:
-                    rel_model_name = record.field_id.relation
-                    model_ids = self.pool.get('ir.model').search(cr, uid, [('model', '=', rel_model_name)])
-                    export_ids = self.pool.get('tk.export.xml').search(cr, uid, [('model_id', 'in', model_ids)])
-                    if export_ids:
-                        res[record.id] = 'run_recursive'
-                    else:
-                        res[record.id] = 'run_classic'
-                else:
-                    res[record.id] = 'run_classic'
-            else:
-                res[record.id] = record.action
-
-        return res
-
-
-    def _set_action(self, cr, uid, ids, name, value, arg, context=None):
-        if isinstance(ids, (int, long)):
-            ids = [ids]
-        if value:
-            print str(value) + '----' + str(ids)
-            query = '''
-                UPDATE tk_field_record set action = %s where id = any(%s)
-            '''
-            cr.execute(query, (value, ids))
-            cr.commit()
-
-            return True
-        else:
-            return False
-
 
     _columns = {
         'export_id': fields.many2one('tk.export.xml', string='Export', required=False),
-        'model_id': fields.many2one('ir.model', string='Model',),
+        'model_id': fields.many2one('ir.model', string='Model', ),
         'field_id': fields.many2one('ir.model.fields', string='Field'),
         'field_name': fields.related('field_id', 'name', string='Name', type='char'),
         'field_relation': fields.related('field_id', 'relation', string='Relation', type='char'),
         'field_type': fields.related('field_id', 'ttype', string='Type', type='char'),
         'field_required': fields.related('field_id', 'required', string='Required', type='boolean'),
-        'action': fields.function(_get_default_action, store=True, string='Action', method=True, type='selection', selection=_actions, fnct_inv=_set_action),
+        'action': fields.selection(_actions, string='Action', required=True),
     }
 
-
+    _defaults = {
+        'action': 'enabled'
+    }
 
 
 tk_field_record()
