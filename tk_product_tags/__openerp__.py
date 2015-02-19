0d774093f02da02bf8f8afb335f2776495815c88
David Lefever <dl@taktik.be>
2015-02-19 10:56:56 +0100

    Added tk_product_tags module.

---
+++ tk_product_tags/__openerp__.py
@@ -0,0 +1,24 @@
+# 
+# 
+# 
+{
+    "name": "Taktik Product Tags",
+    "version": "0.1",
+    "author": "Taktik S.A.",
+    "category": "Generic Modules/Others",
+    "website": "http://www.taktik.be",
+    "description":
+        """
+        This module adds tags to the product variant.
+        """,
+    "depends": [
+        "base", "product"
+    ],
+    "init_xml": [],
+    "demo_xml": [],
+    "data": [
+        'view/tk_product_view.xml',
+    ],
+    "active": False,
+    "installable": True
+}
