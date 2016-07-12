9495cd60ab3a7b8f819bec913cc64e60f0981635
Kevin Broodkoorn <kb@taktik.com>
2016-07-12 14:23:35 +0200

    OTK-944 - tk_fixed_footer

--- tk_fixed_footer/static/src/js/fixed_footer.js
+++ tk_fixed_footer/static/src/js/fixed_footer.js
@@ -1,4 +1,4 @@
-openerp.caro_fixed_footer = function (instance) {
+openerp.tk_fixed_footer = function (instance) {
     var _t = instance.web._t,
         _lt = instance.web._lt;
     var QWeb = instance.web.qweb;
