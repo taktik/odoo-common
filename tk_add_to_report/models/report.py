722b6e894588ca2ce8e2f8428a1c49131f542f2b
Kevin Broodkoorn <kb@taktik.com>
2016-08-03 16:46:56 +0200

    OTK-920 - Conditions Générales de Ventes

--- tk_add_to_report/models/report.py
+++ tk_add_to_report/models/report.py
@@ -29,6 +29,8 @@
 from openerp import api, models
 from base64 import b64decode
 from PyPDF2 import PdfFileMerger, PdfFileReader
+from PyPDF2.generic import ArrayObject, NameObject
+from PyPDF2.utils import isString, PdfReadError
 from io import BytesIO
 from StringIO import StringIO
 
@@ -40,10 +42,32 @@ def _buildOutline(self, node):
     :param node:
     :return:
     """
-    if node.get('/Dest', '') == '/__WKANCHOR_2':
+    dest, title, outline = None, None, None
+
+    if "/A" in node and "/Title" in node:
+        # Action, section 8.5 (only type GoTo supported)
+        title = node["/Title"]
+        action = node["/A"]
+        if action["/S"] == "/GoTo":
+            dest = action["/D"]
+    elif "/Dest" in node and "/Title" in node:
+        # Destination, section 8.2.1
+        title = node["/Title"]
+        dest = node["/Dest"]
+
+    if dest == '/__WKANCHOR_2':
         return None
-    else:
-        return super(PdfFileReader, self)._buildOutline(node)
+
+    # if destination found, then create outline
+    if dest:
+        if isinstance(dest, ArrayObject):
+            outline = self._buildDestination(title, dest)
+        elif isString(dest) and dest in self._namedDests:
+            outline = self._namedDests[dest]
+            outline[NameObject("/Title")] = title
+        else:
+            raise PdfReadError("Unexpected destination %r" % dest)
+    return outline
 
 PdfFileReader._buildOutline = _buildOutline
 
@@ -51,11 +75,12 @@ PdfFileReader._buildOutline = _buildOutline
 class Report(models.Model):
     """
     Report
+    A version of this method exist in ovizio_sale_report.
+    It has changed because of the Odoo version.
     """
     _inherit = 'report'
 
-    @api.multi
-    def get_pdf(self, report_name, html=None, data=None):
+    def get_pdf(self, cr, uid, ids, report_name, html=None, data=None, context=None):
         """
         Check if this report model need to include the 'Term and Condition' (tac) pdf.
         If True, loop on each report and add at the end the tac pdf.
@@ -64,24 +89,37 @@ class Report(models.Model):
         :param data: (dict)
         :return: 'string' with pdf value
         """
-        report_id = self.env['ir.actions.report.xml'].search([
-            ('report_name', '=', report_name)
-        ])
-        if report_id[0].include_term_and_condition_pdf:
+        action_report_obj = self.pool.get('ir.actions.report.xml')
+        report_id = action_report_obj.search(
+            cr,
+            uid,
+            [('report_name', '=', report_name)]
+        )
+        report = action_report_obj.browse(
+            cr,
+            uid,
+            report_id[0],
+            context
+        )
+        if report.include_term_and_condition_pdf:
             tac_binary = b64decode(
-                self.env['res.users'].browse(self._uid).get_sale_term_and_condition()
+                self.pool.get('res.users').browse(
+                    cr, uid, uid, context).get_sale_term_and_condition()
             )
 
             if tac_binary:
                 tac_pdf = PdfFileReader(BytesIO(tac_binary))
                 merger = PdfFileMerger()
 
-                for record in self:
-                    pdf = super(Report, record).get_pdf(
-                        record,
+                for record in self.browse(cr, uid, ids, context=context):
+                    pdf = super(Report, self).get_pdf(
+                        cr,
+                        uid,
+                        record.ids,
                         report_name,
-                        html=html,
-                        data=data
+                        html,
+                        data,
+                        context
                     )
                     merger.append(StringIO(pdf))
 
@@ -91,8 +129,11 @@ class Report(models.Model):
                 merger.write(final_pdf)
                 return final_pdf.getvalue()
         return super(Report, self).get_pdf(
-            self,
+            cr,
+            uid,
+            ids,
             report_name,
-            html=html,
-            data=data
+            html,
+            data,
+            context
         )
