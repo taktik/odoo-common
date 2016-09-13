22d6829e1e80d0a798904eaf3f2ed96a6cc8fc26
Olivier de WITTE <olivier.dewitte@ext.taktik.com>
2016-09-13 13:28:32 +0200

    CARO-905 [FIX] fix js

--- tk_account_move_line_search_extension/static/src/js/account_move_line_search_extension.js
+++ tk_account_move_line_search_extension/static/src/js/account_move_line_search_extension.js
@@ -1,15 +1,14 @@
-openerp.account_move_line_search_extension = function (instance) {
-    var QWeb = instance.web.qweb,
-        _t = instance.web._t;
+openerp.tk_account_move_line_search_extension = function (instance) {
+    var QWeb = instance.web.qweb
+    var _t = instance.web._t;
     var module = instance.account_move_line_search_extension;
 
     instance.account_move_line_search_extension.ListSearchView.include({
        aml_search_domain: function() {
-           console.log('TEST HERITAGE');
-           
-           this.current_amount_min = this.current_amount_min.replace(/[,]/g, '.');
-           this.current_amount_max = this.current_amount_max.replace(/[,]/g, '.');
-
+           if (this.current_amount_min && this.current_amount_max) {
+                this.current_amount_min = this.current_amount_min.replace(/[,]/g, '.');
+                this.current_amount_max = this.current_amount_max.replace(/[,]/g, '.');
+            }
            return this._super();
        },
     });
