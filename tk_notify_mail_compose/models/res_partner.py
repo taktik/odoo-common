9c9427298abc189c41afc5f8726c3663271e9f70
Olivier de WITTE <olivier.dewitte@ext.taktik.com>
2016-09-14 18:40:17 +0200

    [FIX] add new selection

--- tk_notify_mail_compose/models/res_partner.py
+++ tk_notify_mail_compose/models/res_partner.py
@@ -1,18 +1,38 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Author: Taktik S.A.
+#    Copyright (c) 2015 Taktik S.A. (http://www.taktik.be)
+#    All Rights Reserved
+#
+#    WARNING: This program as such is intended to be used by professional
+#    programmers who take the whole responsibility of assessing all potential
+#    consequences resulting from its eventual inadequacies and bugs.
+#    End users who are looking for a ready-to-use solution with commercial
+#    guarantees and support are strongly advised to contact a Free Software
+#    Service Company.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
 from openerp import models, fields, api, _, exceptions
 
 
 class ResPartner(models.Model):
     _name = "res.partner"
-    _inherit = ['res.partner', 'mail.thread']
+    _inherit = 'res.partner'
 
-    def _register_hook(self, cr):
-        """
-        Adds an option in the notify_email.
-        """
-        if 'never_except_mail_compose' not in [x[0] for x in
-                                  self._columns['notify_email'].selection]:
-            self._columns['notify_email'].selection.append(
-                ('never_except_mail_compose',
-                 'Never Except Manual Send By Mail')
-            )
-        return super(ResPartner, self)._register_hook(cr)
+    notify_email = fields.Selection(selection_add=[
+        ('never_except_mail_compose', _('Never Except Manual Send By Mail'))
+    ])
