c9d1a21a84c72ac94db92d24cdc89a391011c95e
Olivier de Witte <odw@taktik.com>
2019-09-26 14:46:58 +0200

    [ADD] add tk_sendgrid_mail_server module

---
+++ tk_sendgrid_mail_server/models/ir_mail_server.py
@@ -0,0 +1,38 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Author: Taktik S.A.
+#    Copyright (c) 2015 Taktik S.A. (http://www.taktik.be)
+#    All Rights Reserved
+#
+#    WARNING: This program as such is intended to be used by professional
+#    programmers who take the whole responsibility of assessing all potential
+#    consequences resulting from its eventual inadequacies and bugs.
+#    End users who are looking for a ready-to-use solution with commercial
+#    guarantees and support are strongly advised to contact a Free Software
+#    Service Company.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+from odoo import api, exceptions, fields, models, _
+
+
+class IrMailServer(models.Model):
+    """
+    Ir Mail Server
+    """
+    _inherit = 'ir.mail_server'
+
+    smtp_pass = fields.Char(string='Password Test', size=256, help="Optional password for SMTP authentication")
